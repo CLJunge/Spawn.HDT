40f7b82b5238030cf27e2fcb6457d69fe815d077
Chris-Lennart Junge <j.chris_lennart@live.de>
2017-10-07 15:59:40 +0200

    Moved Converters namespace to UI namespace

--- Spawn.HDT.DustUtility/UI/Converters/CountToColorConverter.cs
+++ Spawn.HDT.DustUtility/UI/Converters/CountToColorConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace Spawn.HDT.DustUtility.Converters
+namespace Spawn.HDT.DustUtility.UI.Converters
 {
     public class CountToColorConverter : IMultiValueConverter
     {
--- Spawn.HDT.DustUtility/UI/Converters/DisplayLabelConverter.cs
+++ Spawn.HDT.DustUtility/UI/Converters/DisplayLabelConverter.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Spawn.HDT.DustUtility.Converters
+namespace Spawn.HDT.DustUtility.UI.Converters
 {
     public class DisplayLabelConverter : IValueConverter
     {
