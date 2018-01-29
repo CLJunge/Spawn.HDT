897fed76749706ffa0ab336f55d8654e7617aab2
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-01-30 00:58:56 +0100

    Implemented flyout intialization

--- Spawn.HDT.DustUtility/UI/ViewModels/DecksFlyoutViewModel.cs
+++ Spawn.HDT.DustUtility/UI/ViewModels/DecksFlyoutViewModel.cs
@@ -1,11 +1,21 @@
-﻿using GalaSoft.MvvmLight.CommandWpf;
+﻿#region Using
+using GalaSoft.MvvmLight.CommandWpf;
+using HearthMirror.Objects;
+using Spawn.HDT.DustUtility.UI.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+#endregion
 
 namespace Spawn.HDT.DustUtility.UI.ViewModels
 {
     public class DecksFlyoutViewModel : ViewModelBase
     {
         #region Properties
+        #region DeckItems
+        public ObservableCollection<DeckItem> DeckItems { get; set; }
+        #endregion
+
         #region ShowDeckListCommand
         public ICommand ShowDeckListCommand => new RelayCommand(ShowDeckList);
         #endregion
@@ -15,11 +25,25 @@ namespace Spawn.HDT.DustUtility.UI.ViewModels
         #endregion
         #endregion
 
+        #region Ctor
+        public DecksFlyoutViewModel()
+        {
+            DeckItems = new ObservableCollection<DeckItem>();
+        }
+        #endregion
+
         #region Initialize
         public override void Initialize()
         {
             if (ReloadRequired)
             {
+                List<Deck> lstDecks = DustUtilityPlugin.CurrentAccount.GetDecks();
+
+                for (int i = 0; i < lstDecks.Count; i++)
+                {
+                    DeckItems.Add(new DeckItem(lstDecks[i]));
+                }
+
                 ReloadRequired = false;
             }
             else { }
