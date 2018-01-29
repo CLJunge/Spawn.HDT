d833bda03cb9e7f5a3421e1fca928d1e1c55987e
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-01-29 23:12:21 +0100

    Started implementing flyout

--- Spawn.HDT.DustUtility/ViewModel/DecksFlyoutViewModel.cs
+++ Spawn.HDT.DustUtility/ViewModel/DecksFlyoutViewModel.cs
@@ -1,9 +1,39 @@
-﻿namespace Spawn.HDT.DustUtility.ViewModel
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System.Windows.Input;
+
+namespace Spawn.HDT.DustUtility.ViewModel
 {
     public class DecksFlyoutViewModel : ViewModelBase
     {
+        #region Properties
+        #region ShowDeckListCommand
+        public ICommand ShowDeckListCommand => new RelayCommand(ShowDeckList);
+        #endregion
+
+        #region ToggleDeckCommand
+        public ICommand ToggleDeckCommand => new RelayCommand(ToggleDeck);
+        #endregion
+        #endregion
+
         #region Initialize
         public override void Initialize()
+        {
+            if (ReloadRequired)
+            {
+                ReloadRequired = false;
+            }
+            else { }
+        }
+        #endregion
+
+        #region ShowDeckList
+        private void ShowDeckList()
+        {
+        }
+        #endregion
+
+        #region ToggleDeck
+        private void ToggleDeck()
         {
         }
         #endregion
