e8a508178177007d6af8003612134ba88ffe124e
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-01-29 22:25:35 +0100

    Removed NotImplementException

--- Spawn.HDT.DustUtility/ViewModel/DecksFlyoutViewModel.cs
+++ Spawn.HDT.DustUtility/ViewModel/DecksFlyoutViewModel.cs
@@ -1,13 +1,10 @@
-﻿using System;
-
-namespace Spawn.HDT.DustUtility.ViewModel
+﻿namespace Spawn.HDT.DustUtility.ViewModel
 {
     public class DecksFlyoutViewModel : ViewModelBase
     {
         #region Initialize
         public override void Initialize()
         {
-            throw new NotImplementedException();
         }
         #endregion
     }
