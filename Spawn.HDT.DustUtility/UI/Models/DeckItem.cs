242c9c55c9a2dbd91dd14185a11084cf5f41c2aa
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-01-30 01:32:51 +0100

    Implemented GetHeroImage

--- Spawn.HDT.DustUtility/UI/Models/DeckItem.cs
+++ Spawn.HDT.DustUtility/UI/Models/DeckItem.cs
@@ -1,53 +1,122 @@
 ﻿using GalaSoft.MvvmLight;
+using HearthMirror.Objects;
+using System;
+using System.Collections.Generic;
 using System.Windows.Media.Imaging;
 
 namespace Spawn.HDT.DustUtility.UI.Models
 {
     public class DeckItem : ObservableObject
     {
+        #region Static Fields
+        private static Dictionary<string, BitmapImage> s_dImageCache = new Dictionary<string, BitmapImage>();
+        #endregion
+
         #region Member Variables
-        private long m_nDeckId;
-        private string m_strName;
-        private int m_nCardCount;
-        private int m_nCraftingCost;
+        private Deck m_deck;
         #endregion
 
         #region Properties
-        #region DeckId
-        public long DeckId
+        #region Deck
+        public Deck Deck
         {
-            get => m_nDeckId;
-            set => Set(ref m_nDeckId, value);
+            get => m_deck;
+            set => Set(ref m_deck, value);
         }
+        #endregion
+
+        #region DeckId
+        public long DeckId => (Deck?.Id ?? 0);
         #endregion
 
         #region Name
-        public string Name
-        {
-            get => m_strName;
-            set => Set(ref m_strName, value);
-        }
+        public string Name => Deck?.Name;
         #endregion
 
         #region CardCount
-        public int CardCount
-        {
-            get => m_nCardCount;
-            set => Set(ref m_nCardCount, value);
-        }
+        public int CardCount => (Deck?.Cards.Count ?? 0);
         #endregion
 
         #region CraftingCost
-        public int CraftingCost
-        {
-            get => m_nCraftingCost;
-            set => Set(ref m_nCraftingCost, value);
-        }
+        public int CraftingCost => (Deck?.GetCraftingCost() ?? 0);
         #endregion
 
         #region HeroImage
-        public BitmapImage HeroImage { get; set; }
+        public BitmapImage HeroImage => GetHeroImage();
         #endregion
+        #endregion
+
+        #region Ctor
+        public DeckItem()
+        {
+        }
+
+        public DeckItem(Deck deck)
+        {
+            m_deck = deck;
+        }
+        #endregion
+
+        #region GetHeroImage
+        private BitmapImage GetHeroImage()
+        {
+            string strHero = Deck?.Hero.Substring(0, 7);
+
+            if (!s_dImageCache.TryGetValue(strHero, out BitmapImage retVal))
+            {
+                string strSource = string.Empty;
+
+                switch (strHero)
+                {
+                    case "HERO_01": //Warrior
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_garrosh.png";
+                        break;
+
+                    case "HERO_02": //Shaman
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_thrall.png";
+                        break;
+
+                    case "HERO_03": //Rogue
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_valeera.png";
+                        break;
+
+                    case "HERO_04": //Paladin
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_uther.png";
+                        break;
+
+                    case "HERO_05": //Hunter
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_rexxar.png";
+                        break;
+
+                    case "HERO_06": //Druid
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_malfurion.png";
+                        break;
+
+                    case "HERO_07": //Warlock
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_guldan.png";
+                        break;
+
+                    case "HERO_08": //Mage
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_jaina.png";
+                        break;
+
+                    case "HERO_09": //Priest
+                        strSource = "/Spawn.HDT.DustUtility;component/Resources/Images/hero_anduin.png";
+                        break;
+                }
+
+                if (!string.IsNullOrEmpty(strSource))
+                {
+                    retVal = new BitmapImage(new Uri(strSource, UriKind.Relative));
+
+                    s_dImageCache.Add(strHero, retVal);
+                }
+                else { }
+            }
+            else { }
+
+            return retVal;
+        }
         #endregion
     }
 }
