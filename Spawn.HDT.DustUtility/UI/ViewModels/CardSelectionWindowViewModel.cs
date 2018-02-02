335978ed39a1ee5f1b6c4a1aa46a71120c8fede4
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-02-02 09:02:15 +0100

    Added CardSelectionWindow

---
+++ Spawn.HDT.DustUtility/UI/ViewModels/CardSelectionWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Spawn.HDT.DustUtility.UI.Models;
+using System.Collections.ObjectModel;
+
+namespace Spawn.HDT.DustUtility.UI.ViewModels
+{
+    public class CardSelectionWindowViewModel : ViewModelBase
+    {
+        #region Member Variables
+        private string m_strWindowTitle;
+        #endregion
+
+        #region Properties
+        #region WindowTitle
+        public string WindowTitle
+        {
+            get => m_strWindowTitle;
+            set => Set(ref m_strWindowTitle, value);
+        }
+        #endregion
+
+        #region CardItems
+        public ObservableCollection<CardItemModel> CardItems { get; set; }
+        #endregion
+
+        #region CardsInfo
+        public CardsInfoModel CardsInfo { get; set; }
+        #endregion
+        #endregion
+
+        #region Ctor
+        public CardSelectionWindowViewModel()
+        {
+            WindowTitle = "Dust Utility - Selection";
+
+            CardItems = new ObservableCollection<CardItemModel>();
+
+            CardsInfo = new CardsInfoModel();
+        }
+        #endregion
+
+        #region Initialize
+        public override void Initialize()
+        {
+        }
+        #endregion
+    }
+}
---
+++ Spawn.HDT.DustUtility/UI/Windows/CardSelectionWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Spawn.HDT.DustUtility.UI.Windows
+{
+    public partial class CardSelectionWindow
+    {
+        #region Ctor
+        public CardSelectionWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        #region OnRemoveCardItem
+        private void OnRemoveCardItem(object sender, CardItemEventArgs e)
+        {
+        }
+        #endregion
+
+        #region OnItemDropped
+        private void OnItemDropped(object sender, CardItemEventArgs e)
+        {
+        }
+        #endregion
+        #endregion
+    }
+}
