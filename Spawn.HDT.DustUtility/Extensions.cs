92730c75546429bc978e80a92b961db22736f32a
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-05-30 23:22:24 +0200

    Add GetDisplayName method

--- Spawn.HDT.DustUtility/Extensions.cs
+++ Spawn.HDT.DustUtility/Extensions.cs
@@ -142,6 +142,11 @@ namespace Spawn.HDT.DustUtility
             return CardSets.All[cardSet];
         }
 
+        public static string GetDisplayString(this CardSet cardSet)
+        {
+            return Cards.AllDisplayName[cardSet];
+        }
+
         public static string GetShortString(this CardSet cardSet)
         {
             return CardSets.AllShortName[cardSet];
