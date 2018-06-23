d381a44cf928a0f87ba61a02510d2dab4a48a3e5
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-06-23 06:50:42 +0200

    Hide disenchanting button appropriately

---
+++ Spawn.HDT.DustUtility/UI/Components/Converters/DisenchantButtonVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿#region Using
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+#endregion
+
+namespace Spawn.HDT.DustUtility.UI.Components.Converters
+{
+    public class DisenchantButtonVisibilityConverter : IMultiValueConverter
+    {
+        #region Convert
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility retVal = Visibility.Hidden;
+
+            if (values.Length == 2 && (Visibility)values[0] == Visibility.Visible && (Visibility)values[1] == Visibility.Visible)
+            {
+                retVal = Visibility.Visible;
+            }
+            else { }
+
+            return retVal;
+        }
+        #endregion
+
+        #region ConvertBack
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+        #endregion
+    }
+}
