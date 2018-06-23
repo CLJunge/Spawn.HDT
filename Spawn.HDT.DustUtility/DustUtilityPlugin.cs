f2151817134b7774dc6fd35e80f6ed9c5d33918f
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-06-24 00:11:54 +0200

    Fixed null ref exception

--- Spawn.HDT.DustUtility/DustUtilityPlugin.cs
+++ Spawn.HDT.DustUtility/DustUtilityPlugin.cs
@@ -304,7 +304,7 @@ namespace Spawn.HDT.DustUtility
 
             IAccount loggedInAcc = await Account.GetLoggedInAccountAsync();
 
-            if (loggedInAcc != null)
+            if (loggedInAcc?.IsValid ?? false)
             {
                 if (!IsOffline)
                 {
