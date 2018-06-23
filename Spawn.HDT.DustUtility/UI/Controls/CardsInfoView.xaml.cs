2004bf28deb8fe0b2e4cf82ca2e97f86beeb7a65
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-06-23 04:05:47 +0200

    Added Title depedency property

--- Spawn.HDT.DustUtility/UI/Controls/CardsInfoView.xaml.cs
+++ Spawn.HDT.DustUtility/UI/Controls/CardsInfoView.xaml.cs
@@ -1,12 +1,28 @@
-﻿namespace Spawn.HDT.DustUtility.UI.Controls
+﻿#region Using
+using System.Windows;
+#endregion
+
+namespace Spawn.HDT.DustUtility.UI.Controls
 {
     public partial class CardsInfoView
     {
+        #region DP Title
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(CardsInfoView), new PropertyMetadata("Info"));
+        #endregion
+
         #region Ctor
         public CardsInfoView()
         {
             InitializeComponent();
-        } 
+        }
         #endregion
     }
 }
