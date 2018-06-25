8f7e15261e16434e828dbb18bdeffe7043f81464
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-06-25 22:58:25 +0200

    Code refactor

--- Spawn.HDT.DustUtility/Logging/FileAccessException.cs
+++ Spawn.HDT.DustUtility/Logging/FileAccessException.cs
@@ -4,12 +4,22 @@ using System;
 
 namespace Spawn.HDT.DustUtility.Logging
 {
+    [Serializable]
     public class FileAccessException : Exception
     {
         #region Ctor
-        public FileAccessException(string Message, Exception InnerException) : base(Message, InnerException)
+        public FileAccessException()
         {
         }
+        public FileAccessException(string message) : base(message)
+        {
+        }
+        public FileAccessException(string message, Exception inner) : base(message, inner)
+        {
+        }
+        protected FileAccessException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
         #endregion
     }
 }
