ce5e1c2b46485abf1ddbe783d769c3854a041e79
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-06-26 00:54:56 +0200

    Updated msg box title

--- Spawn.HDT.DustUtility/UI/Dialogs/SettingsDialogView.xaml.cs
+++ Spawn.HDT.DustUtility/UI/Dialogs/SettingsDialogView.xaml.cs
@@ -24,7 +24,7 @@ namespace Spawn.HDT.DustUtility.UI.Dialogs
         #region OnAutoDisenchantingCheckBoxChecked
         private void OnAutoDisenchantingCheckBoxChecked(object sender, RoutedEventArgs e)
         {
-            if (Visibility == Visibility.Visible && MessageBox.Show("You are using this feature at your own risk!\r\n\r\nThere is always a slight chance, that the wrong card might get disenchanted.\r\n\r\nAre you sure you want to enable auto disenchanting?", "Dust Utility", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+            if (Visibility == Visibility.Visible && MessageBox.Show("You are using this feature at your own risk!\r\n\r\nThere is always a slight chance, that the wrong card might get disenchanted.\r\n\r\nAre you sure you want to enable auto disenchanting?", "Dust Utility - Auto Disenchanting", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
             {
                 AutoDisenchantingCheckBox.IsChecked = false;
             }
