5d4aba51ce234b9b2157950e573a43ed83ad76df
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-06-27 06:45:15 +0200

    Added description attributes to TimeUnit

--- Spawn.HDT.DustUtility/Util/TimeUnit.cs
+++ Spawn.HDT.DustUtility/Util/TimeUnit.cs
@@ -1,8 +1,14 @@
-﻿namespace Spawn.HDT.DustUtility.Util
+﻿#region Using
+using System.ComponentModel;
+#endregion
+
+namespace Spawn.HDT.DustUtility.Util
 {
     public enum TimeUnit
     {
+        [Description("s")]
         Seconds,
+        [Description("min")]
         Minutes,
     }
 }
