ba75da4679a8798ac3b5645b1b09a80cbf82f157
Chris-Lennart Junge <j.chris_lennart@live.de>
2018-08-29 22:25:01 +0200

    [skip ci] Updated assembly info

--- Spawn.HDT.DustUtility/Properties/AssemblyInfo.cs
+++ Spawn.HDT.DustUtility/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("Spawn.HDT.DustUtility")]
-[assembly: AssemblyDescription("A plugin for card management for the Hearthstone Deck Tracker")]
+[assembly: AssemblyDescription("A  collection management plugin for the Hearthstone Deck Tracker")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Spawn Development")]
 [assembly: AssemblyProduct("Spawn.HDT.DustUtility")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
