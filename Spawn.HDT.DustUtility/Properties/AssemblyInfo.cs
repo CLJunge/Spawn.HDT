586ddaed9d406612781dbccc45efaa99ef1ca60c
Chris-Lennart Junge <j.chris_lennart@ymail.com>
2020-01-25 20:01:51 +0100

    v3.4.0

--- Spawn.HDT.DustUtility/Properties/AssemblyInfo.cs
+++ Spawn.HDT.DustUtility/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
